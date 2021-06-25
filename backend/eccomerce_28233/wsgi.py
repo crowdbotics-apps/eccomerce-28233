bf7ce43258cd4816fa60dcd913906668817c72cb
Crowdbotics <team@crowdbotics.com>
2021-06-25 12:51:58 +0000

    Initial commit.

---
+++ backend/eccomerce_28233/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for eccomerce_28233 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eccomerce_28233.settings')
+
+application = get_wsgi_application()
